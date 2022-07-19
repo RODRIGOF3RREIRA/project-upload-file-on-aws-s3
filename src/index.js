b1ca8c97512e8a4c65097d9f1962226e7c410896
RODRIGOF3RREIRA <rodrigoferreira2799@gmail.com>
2022-07-19 15:24:33 -0400

    chore: add environment variables and express static

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/upload', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -14,6 +15,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/upload', {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')),
+);
 
 app.use(require('./routes'));
 
