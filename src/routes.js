f1432b80b7733321fd017b21bb09a7b7f600dd8a
RODRIGOF3RREIRA <rodrigoferreira2799@gmail.com>
2022-07-19 10:29:09 -0400

    chore: add url field

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,12 @@ const multerConfig = require('./config/multer');
 const Post = require('./models/Post');
 
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
+  const { originalname: name, size, key, url = '' } = req.file;
   const post = await Post.create({
-    name: req.file.originalname,
-    size: req.file.size,
-    key: req.file.filename,
-    url: '',
+    name,
+    size,
+    key,
+    url,
   });
   return res.json(post);
 });
